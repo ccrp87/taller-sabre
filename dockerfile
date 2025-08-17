395390c9b85d71c2cea39e45defdbcedfe30b712
Carlos C. Romero Pertuz <romeropertuzcarlos@gmail.com>
2025-08-17 10:33:29 -0500

    se configuran cors

--- dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.12.3-slim-bullseye
-WORKDIR /app
-COPY requirements.txt .
-RUN apt-get update && apt-get upgrade -y && apt-get clean && rm -rf /var/lib/apt/lists/* \
-	&& pip install --no-cache-dir -r requirements.txt
-COPY . .
-EXPOSE 8000
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from api.reservation_router import reservation_router
 from api.room_router import room_router
@@ -13,6 +14,22 @@ async def lifespan(app:FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="Hotel Reservation API", version="1.0.0")
+
+origins = [
+    "https://brave-wave-0ff2b8a0f.1.azurestaticapps.net",
+    "https://myapp.3itc.co",
+    "https://myapp2.3itc.co",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(reservation_router)
 app.include_router(room_router)
 
